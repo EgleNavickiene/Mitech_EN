f08cfbbfa418503980024769a28448d4806db230
EgleNavickiene <navickiene@gmail.com>
2021-06-26 09:35:44 +0300

    keiciau bet grazinau atgal
    uzkomentavau bootstrap karusele

--- assets/js/mitech-script.js
+++ assets/js/mitech-script.js
@@ -1,9 +1,20 @@
-$('.slick-slider').slick({
-  infinite: true,
-  speed: 300,
-  slidesToShow: 3,
-  centerMode: true,
-  variableWidth: true,
-  autoplay: true,
-  autoplaySpeed: 800,
+$(document).ready(function() {
+
+  $('.slick-slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 800,
+    });
+
+  $('.autoplay').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
   });
+});
+
